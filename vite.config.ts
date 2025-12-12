71f4eec9e146cf82e77a6e84e50cfc067ed1cdbb
PraneetNS <savantpraneet@gmail.com>
2025-12-12 23:20:14 +0530

    added files of frontend

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/impact": "http://127.0.0.1:8000",
+      "/chat": "http://127.0.0.1:8000",
+      "/repos": "http://127.0.0.1:8000",
+      "/search": "http://127.0.0.1:8000"
+    }
+  }
+});
